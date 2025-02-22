31119cbc589acdb7d7ddd18460e651007aec80ba
Pavel9125 <penchev95@gmail.com>
2025-02-22 13:20:59 +0000

    week 1 Hello and test

--- Week 1 - Introduction/Input.py
+++ Week 1 - Introduction/Input.py
@@ -4,4 +4,7 @@ firstname = input("Please enter your firstname:")
 numLucky = input("Please enter your lucky number: ")
 print("your full name is :", surname, firstname)
 print("Your lucky number is :", numLucky)
+print("Your unique password is:", surname+numLucky)
+print(f"Your unique password is: {surname}{numLucky}")
+
 
