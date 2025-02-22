0c82c02e075e796d246203880cae5a493bd07724
Pavel9125 <penchev95@gmail.com>
2025-02-22 12:33:48 +0000

    week 1 Hello and test

--- Week 1 - Introduction/output_tasks.py
+++ Week 1 - Introduction/output_tasks.py
@@ -8,9 +8,9 @@ print("In week 1 we will learn...")
 # Activity 2 - Task 2
 print("Welcome to QHO426!")
 print("In week 1 we will learn...")
-
+print("")
 print("...How to use Git and GitHub")
 print("...How to Output on the screen")
 print("...How to get user Input")
-
+print("")
 print("I hope you are enjoying the lesson thus far!")
