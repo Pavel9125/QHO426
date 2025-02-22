ae86e21830bfa229dc05aa4373c3d4fc4e31c53b
Pavel9125 <penchev95@gmail.com>
2025-02-22 13:14:01 +0000

    week 1 Hello and test

--- Week 1 - Introduction/Input.py
+++ Week 1 - Introduction/Input.py
@@ -1,7 +1,7 @@
 #Input
 surname = input("Please enter your surname:")
 firstname = input("Please enter your firstname:")
-print("your full name is :", surname, firstname)
 numLucky = input("Please enter your lucky number: ")
+print("your full name is :", surname, firstname)
 print("Your lucky number is :", numLucky)
 
