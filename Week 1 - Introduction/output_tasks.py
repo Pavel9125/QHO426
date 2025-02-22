3e4e5f46ffc6a7325ca0d3436ecff00e60f39d96
Pavel9125 <penchev95@gmail.com>
2025-02-22 12:44:36 +0000

    week 1 Hello and test

--- Week 1 - Introduction/output_tasks.py
+++ Week 1 - Introduction/output_tasks.py
@@ -1,11 +1,13 @@
-# Activity 1
-print("Welcome to QHO426")
+# Activity 1 Outputting a Simple Message
+print("Welcome to \"QHO426!\"")
 
-# Activity 2 - Task 1
-print("Welcome to QHO426!")
+# Activity 2 Outputting a Multi-line Message
+# Task 1
+print("Welcome to 'QHO426!'")
 print("In week 1 we will learn...")
 
-# Activity 2 - Task 2
+# Activity 2 Outputting a Multi-line Message
+# Task 2
 print("Welcome to QHO426!")
 print("In week 1 we will learn...")
 print("")
@@ -14,3 +16,11 @@ print("...How to Output on the screen")
 print("...How to get user Input")
 print("")
 print("I hope you are enjoying the lesson thus far!")
+
+# Activity 3 Escape Characters
+# Task 1
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
