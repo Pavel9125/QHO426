3b49b8990dd584b590db84f4f39795f3bb8c36f7
Pavel9125 <penchev95@gmail.com>
2025-02-26 19:21:05 +0000

    1

--- Week 1 - Introduction/input_tasks.py
+++ Week 1 - Introduction/input_tasks.py
@@ -27,9 +27,12 @@ print("How tall are you?(in meters)")
 height = float(input())
 print("How much do you weigh?(in kilograms)")
 weight = float(input())
-BMI= weight / (height ** 2)
+BMI1 = weight / (height **2)# this will show the whole number
+BMI= weight / (height ** 2) # this and the next 2 lines will make the number round to 2 decimal numbers
 BMI_rounded2 = round(BMI, 2)
+BMI= round( (weight / (height **2)), 2)
 print(f"{name} you are is {age} years old and your BMI is {BMI_rounded2} ")
-
+print(f"{name} you are is {age} years old and your BMI is {BMI} ")
+print(f"{name} you are is {age} years old and your BMI is {BMI1} ")
 
 
