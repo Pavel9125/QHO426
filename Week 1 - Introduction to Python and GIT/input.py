8497a9e178174b45345c822910e3a11b4516b35d
Pavel9125 <penchev95@gmail.com>
2025-03-01 12:36:06 +0000

    Test code with Hema

--- Week 1 - Introduction to Python and GIT/input.py
+++ Week 1 - Introduction to Python and GIT/input.py
@@ -1,13 +1,25 @@
 #Input
 surname = input("Please enter your surname:")
 firstname = input("Please enter your firstname:")
-numLucky = int(input("Please enter your lucky number: "))
+numLucky = float(input("Please enter your lucky number: "))
 #Let's add 10 to the lucky number
-numNew = numLucky+10
-print(numNew)
-print("your full name is :", surname, firstname)
-print("Your lucky number is :", numLucky)
-print("Your unique password is:", surname,numLucky)
-print(f"Your unique password is: {surname}{numLucky}")
+#numNew = numLucky+10
+#print(numNew)
+#print("your full name is :", surname, firstname)
+#print("Your lucky number is :", numLucky)
+#print("Your unique password is:", surname,numLucky)
+#print(f"Your unique password is: {surname}{numLucky}")
+if numLucky >= 37:
+    print("Your number is greater than 37.")
+elif numLucky == 500:
+    print("Your number is 37.")
+else:
+    print("Your number is less than 37.")
+
+
+
+
+
+
 
 
