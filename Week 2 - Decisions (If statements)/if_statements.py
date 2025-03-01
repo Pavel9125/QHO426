06245f10fac1183b12a5a09b3e3657f5d4b9d2f5
Pavel9125 <penchev95@gmail.com>
2025-03-01 13:23:51 +0000

    .

--- Week 2 - Decisions (If statements)/if_statements.py
+++ Week 2 - Decisions (If statements)/if_statements.py
@@ -1,6 +1,23 @@
+#Activity 1: IF statement
 #Ask user what type of book
 print("What type of book is this?")
 book = input()
 if book == "adventure":
     print("I like adventure books!")
 print("I have finished reading this book.")
+
+#Activity 2: IF...ELSE statement
+
+
+#Activity 3: IF...ELIF...ELSE statement
+
+
+
+#Activity 4: Modulo Operator
+number_1=int(input("Press Enter a whole number."))
+if number_1%2==0:
+    print("The number is even.")
+else :
+    print("The number is odd.")
+
+#Activity 5: Comparison Operators
