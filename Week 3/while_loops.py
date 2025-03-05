f06791e44268c1eac2564d8443f4869ba42a6390
Pavel9125 <penchev95@gmail.com>
2025-03-05 18:33:01 +0000

    "Added code to demonstrate the use of a simple while loop."

---
+++ Week 3/while_loops.py
@@ -0,0 +1,9 @@
+print("How many apples should I remove?")
+user_remove = int(input())
+removed = 0
+print()
+while removed < user_remove:
+    print(f"{removed+1} Removed Apple")
+    removed = removed +1
+
+
