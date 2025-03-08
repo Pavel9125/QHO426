1ee4951fc115b0b8616765385fdb44b6e3cf2410
Pavel9125 <penchev95@gmail.com>
2025-03-08 13:04:15 +0000

    "Added code to demonstrate the use of a while loop in performing calculations with user input.".

--- Week 3/while_loops.py
+++ Week 3/while_loops.py
@@ -56,6 +56,19 @@ while i <= 100:
 
 print("...Done! The answer is", total)
 
+#Activity 6: Sum User Numbers
+#Task
+number_count = int(input("How many numbers should I sum up?\n"))
+total_sum = 0
+current_number = 1
+while current_number <= number_count:
+    num = int(input(f"Please enter number {current_number} of {number_count}: "))
+    total_sum += num
+    current_number += 1
+print("\nThe answer is", total_sum)
+
+
+
 
 
 
