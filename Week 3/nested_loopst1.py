0edfa171241cf93bd97adf22a461c6af2c01678b
Pavel9125 <penchev95@gmail.com>
2025-03-12 18:19:21 +0000

    "Added code to demonstrate the use of one loop nested is another.".

---
+++ Week 3/nested_loopst1.py
@@ -0,0 +1,12 @@
+print("How many Rows would you like?: ")
+rows = int(input())
+print("How many Columns would you like?: ")
+columns = int(input())
+print("Here I go: \n")
+for row in range(0, rows, 1):
+    for column in range(0, columns, 1):
+        print(":)", end=" ")
+    print()
+print("Done!")
+
+
--- week 4/ufpujb.py
+++ week 4/ufpujb.py
@@ -1,8 +1,18 @@
-number_count = int(input("How many numbers should I sum up?\n"))
-total_sum = 0
-current_number = 1
-while current_number <= number_count:
-    num = float(input(f"Please enter number {current_number} of {number_count}:\n"))
-    total_sum += num
-    current_number += 1
-print("\nThe answer is", total_sum)
+#number_count = int(input("How many numbers should I sum up?\n"))
+#total_sum = 0
+#current_number = 1
+#while current_number <= number_count:
+   # num = float(input(f"Please enter number {current_number} of {number_count}:\n"))
+  #  total_sum += num
+ #   current_number += 1
+#print("\nThe answer is", total_sum)
+
+
+weight = input("Weight: ")
+input ("(K)g or (L)bs: ")
+if input() == "k" or "K":
+    print("\n Your weight in kg is ", weight)
+elif weight.lower() == "l" or "L":
+    print("\n Your weight in lbs is ", weight)
+else:
+    print("\n Wrong input")
