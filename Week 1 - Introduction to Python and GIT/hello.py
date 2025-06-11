0bf853a68a508b0b0beb7931714c332004dec71d
Pavel9125 <penchev95@gmail.com>
2025-06-11 12:15:30 +0100

    Added code to demonstrate the use of an if...else statement.

--- Week 1 - Introduction to Python and GIT/hello.py
+++ Week 1 - Introduction to Python and GIT/hello.py
@@ -3,4 +3,4 @@ print('''This is my week 1 of QHO426
 I am excited to learn Python
 We are going to be learning about Output and Input
 ''')
-print('All about comment') # This is inline comment
+print('All about comment') # This is an inline comment
