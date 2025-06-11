b5d20dc3f2fc270b5334a945a3bf72bc7c9cf019
Pavel9125 <penchev95@gmail.com>
2025-06-11 13:09:55 +0100

    Added code to demonstrate how to nest tuples and lists.

--- Week 5/list_task_5.py
+++
@@ -1,8 +0,0 @@
-def directions():
-    step=["Move fwd" ,10, "Move bwd",5, "Turn Left"
-    return step
-def menuandinput():
-    print( "Please select the direction")
-    for in -
-print(i,path1[i] choice=input()
-def run task4(): route=[] print( "Working on escape route") menuandinput() for in range(0,len()) I
