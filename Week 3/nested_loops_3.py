f52412839d0659e2b5e05ee34c3d2a9a2ee20462
Pavel9125 <penchev95@gmail.com>
2025-06-11 12:53:39 +0100

    Added code to demonstrate the use of a logical and operator.

---
+++ Week 3/nested_loops_3.py
@@ -0,0 +1,19 @@
+print("Please enter a sequence:")
+sequence = input()
+
+print("Please enter the character for the marker:")
+marker = input()
+
+is_counting = False
+count = 0
+
+for character in sequence:
+    if (is_counting == False) and (character == marker):
+      print("Found first marker")
+      is_counting = True
+    elif (is_counting == True) and (character == marker):
+      print("Found last marker")
+    elif is_counting:
+      count += 1
+
+print(f"The distance between the markers is {count}")
